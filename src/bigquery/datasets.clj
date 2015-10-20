d86599fac7094f0d2ab134b710a46caf2782d40e
Paul Ingles <paul@oobaloo.co.uk>
2015-10-20 15:28:29 +0100

    add fns to insert and delete datasets

--- src/bigquery/datasets.clj
+++ src/bigquery/datasets.clj
@@ -1,17 +1,23 @@
 (ns bigquery.datasets
   (:import [com.google.api.services.bigquery Bigquery Bigquery$Datasets]
-           [com.google.api.services.bigquery.model DatasetList$Datasets DatasetReference]))
+           [com.google.api.services.bigquery.model Dataset DatasetList$Datasets DatasetReference]))
 
 (defprotocol ToClojure
   (to-clojure [x]))
 
 (extend-protocol ToClojure
+  Dataset
+  (to-clojure [ds]
+    {:id            (.getId ds)
+     :friendly-name (.getFriendlyName ds)
+     :description   (.getDescription ds)
+     :reference     (.getDatasetReference ds)})
   DatasetReference
   (to-clojure [ref] {:dataset-id (.getDatasetId ref)
                      :project-id (.getProjectId ref)})
   DatasetList$Datasets
   (to-clojure [ds] {:id (.getId ds)
-                    :name (.getFriendlyName ds)
+                    :friendly-name (.getFriendlyName ds)
                     :reference (to-clojure (.getDatasetReference ds))}))
 
 (defn list [^Bigquery service project-id]
@@ -19,3 +25,17 @@
     (->> (.execute list-op)
          (.getDatasets)
          (map to-clojure))))
+
+(defn insert [^Bigquery service project-id {:keys [id friendly-name description] :as dataset}]
+  {:pre [(not (nil? id))]}
+  (let [dataset   (doto (Dataset. )
+                    (.setDatasetReference (doto (DatasetReference. )
+                                            (.setDatasetId id)))
+                    (.setFriendlyName friendly-name)
+                    (.setDescription description))
+        insert-op (-> service (.datasets) (.insert project-id dataset))]
+    (to-clojure (.execute insert-op))))
+
+(defn delete [^Bigquery service project-id dataset-id]
+  (let [delete-op (-> service (.datasets) (.delete project-id dataset-id))]
+    (.execute delete-op)))
