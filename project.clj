a437101a81c95c42539e4911a777f908b9e6f3d2
Paul Ingles <paul@oobaloo.co.uk>
2015-10-20 14:56:23 +0100

    add auth ns

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
 (defproject bigquery "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "BijQuery"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [com.google.api-client/google-api-client "1.20.0"]
+                 [com.google.apis/google-api-services-bigquery "v2-rev238-1.20.0"]])
