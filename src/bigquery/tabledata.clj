5827d249e9ccfa68f29ec1a6833de2ae0269286a
Paul Ingles <paul@oobaloo.co.uk>
2015-10-21 15:57:52 +0100

    got a rudimentary insert working

--- src/bigquery/tabledata.clj
+++ src/bigquery/tabledata.clj
@@ -1,10 +1,19 @@
 (ns bigquery.tabledata
   (:import [com.google.api.services.bigquery Bigquery Bigquery$Tabledata]
-           [com.google.api.services.bigquery.model Table TableDataInsertAllRequest TableDataInsertAllRequest$Rows])
+           [com.google.api.services.bigquery.model Table TableDataInsertAllRequest TableDataInsertAllRequest$Rows TableRow])
   (:require [schema.core :as s]))
 
+
+(defn- mk-table-row [row]
+  (let [table-row (TableRow.)]
+    (doseq [keyval (seq row)]
+      (.set table-row (key keyval) (val keyval)))
+
+    table-row))
+
 (defn insert-all [^Bigquery service project-id dataset-id table-id row]
-  (let [table-data-row (.setJson (TableDataInsertAllRequest$Rows. ) row)
+  (let [row            ( mk-table-row row)
+        table-data-row (.setJson (TableDataInsertAllRequest$Rows. ) row)
         insert-request (.setRows (TableDataInsertAllRequest. ) [table-data-row])
         op (-> service
                (.tabledata)
