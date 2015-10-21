b0a63722cf2f37080adcb5a41a37bc7e9042b9e4
Paul Ingles <paul@oobaloo.co.uk>
2015-10-21 14:27:32 +0100

    Introduced TableDataInsertAllRequest.Rows() class

--- src/bigquery/tabledata.clj
+++ src/bigquery/tabledata.clj
@@ -1,10 +1,11 @@
 (ns bigquery.tabledata
   (:import [com.google.api.services.bigquery Bigquery Bigquery$Tabledata]
-           [com.google.api.services.bigquery.model Table TableDataInsertAllRequest])
+           [com.google.api.services.bigquery.model Table TableDataInsertAllRequest TableDataInsertAllRequest$Rows])
   (:require [schema.core :as s]))
 
 (defn insert-all [^Bigquery service project-id dataset-id table-id row]
-  (let [insert-request (.setRows (TableDataInsertAllRequest. ) [row])
+  (let [table-data-row (.setJson (TableDataInsertAllRequest$Rows. ) row)
+        insert-request (.setRows (TableDataInsertAllRequest. ) [table-data-row])
         op (-> service
                (.tabledata)
                (.insertAll project-id dataset-id table-id insert-request))]
